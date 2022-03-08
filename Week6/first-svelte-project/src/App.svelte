bf8c902c5356dff328434878cee910ae6dca87e4
jona881a <jonaskunert@hotmail.com>
2022-03-08 15:29:00 +0100

    Made Apple in svelte

--- Week6/first-svelte-project/src/App.svelte
+++ Week6/first-svelte-project/src/App.svelte
@@ -1,10 +1,14 @@
 <script>
+import Tree from "./pages/Tree/Tree.svelte";
+
 	export let name;
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Tree/>
 </main>
 
 <style>
---
+++ Week6/first-svelte-project/src/pages/Tree/Tree.svelte
@@ -0,0 +1,19 @@
+<script>
+import Apple from "./Apple.svelte";
+
+  let apples = ["🍎","🍏","🍎","🍏","🍎","🍏"];
+
+  function pickApple() {
+    apples.pop();
+    apples = apples;
+  }
+
+</script>
+
+<h2>I'm a Tree</h2>
+
+{#each apples as apple}
+  <Apple apple={apple}/>
+{/each}
+
+<button on:click={pickApple}>Pick an apple</button> 
